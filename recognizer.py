1830462535d335488a71d0ece5d574a71a70449d
Noah Myers <alphazazoo@gmail.com>
2021-07-29 23:49:50 -0700

    inital commit

---
+++ recognizer.py
@@ -0,0 +1,12 @@
+import fastbook
+fastbook.setup_book()
+from fastbook import *
+from fastai.vision.widgets import *
+
+print(Path())
+# print(path.ls(file_exts='.pkl'))
+
+# learn_inf = load_learner(path/'export.pkl')
+
+
+# print(learn_inf.predict('minions\\cannon\\red\\red_cannon1.jpg'))
