a48b9cca6f050c6fdf256d2c3a0c6f3fff1c83d9
Noah Myers <alphazazoo@gmail.com>
2021-07-30 14:44:01 -0700

    clean up probability display

--- recognizer.py
+++ recognizer.py
@@ -3,4 +3,9 @@ from fastai.vision.all import *
 learn_inf = load_learner('C:\\Users\\Noah\\Desktop\\fastai\\minion recognizer\\export.pkl')
 
 
-learn_inf.predict('C:\\Users\\Noah\\Desktop\\fastai\\minion recognizer\\minions\\test.png')
+pred, pred_idx, probs = learn_inf.predict('C:\\Users\\Noah\\Desktop\\fastai\\minion recognizer\\minions\\test.png')
+
+probability = float(f'{probs[pred_idx]:.4f}') * 100 
+probability = str(f'{probability:.2f}')
+
+print(f'Prediction: {pred}; Confidence: {probability}%')
